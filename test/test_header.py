1f8c82007bcc1e72b2bc72061375c28371c5f58e
noob <noob@noob.noob>
2022-10-22 04:00:24 +0300

    fix

--- test/test_header.py
+++ test/test_header.py
@@ -25,13 +25,14 @@ class TestHeader:
 
 
     @allure.description('Проверка редиректа по клику на логотип "Самокат"')
+    @allure.title('Самокат logo')
     def test_regirect_to_mainpage(self):
         page = self.page
         page.check_redirect_to_mainpage()
 
 
-    @allure.description('Неосилил тест, дзен блокиркует ботов')
-    @allure.title('Неосилил')
+    @allure.description('Проверка перехода на сайт яндекса')
+    @allure.title('Яндекс logo')
     def test_1(self):
         page = self.page
         page.check_redirect_to_yandex()
